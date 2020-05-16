8c5949bf8ab7e288596ce6eb5bbaf6027b999c44
Jaler Dio Fadhilah <37465606+jalerdio@users.noreply.github.com>
2020-05-16 23:52:47 +0700

    init tugas 1

---
+++ Tugas1/1b/client-2.py
@@ -0,0 +1,24 @@
+import sys
+import socket
+SIZE = 100
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect the socket to the port where the server is listening
+port = 31001
+server_address = ('localhost', port)
+print(f"connecting to {server_address}")
+sock.connect(server_address)
+try:
+    # Send Request
+    message = 'message_send.txt'
+    print(f"sending {message} as request")
+    sock.sendall(message.encode())
+    # Look response
+    respon_received = open("received.txt", "wb")
+    if not respon_received:
+        print("Request Rejected\n")
+    respon_received.write(sock.recv(SIZE))
+    print("Request Accepted\n")
+
+finally:
+    sock.close()
---
+++ Tugas1/1b/server-2.py
@@ -0,0 +1,32 @@
+import sys
+import socket
+SIZE = 100
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to the port
+port = 31001
+server_address = ('localhost', port)
+print("starting up")
+print(f"connection from {server_address}")
+# Menghubungkan alamat ip dengan port x ke socket
+sock.bind(server_address)
+# Listen for incoming connections
+sock.listen(1)
+while True:
+    # Wait for a connection
+    print("waiting for a connection")
+    connection, client_address = sock.accept()
+    print(f"connection from {client_address}")
+    # Accept Request
+    request = connection.recv(SIZE)
+    client_request = open(request.decode(),"rb")
+    print("Request Accepted")
+    # Receive the data in small chunks and retransmit it
+    while True:
+        received = client_request.read(SIZE)
+        if not received :
+            break
+        connection.sendall(received)
+        print("Send Data")
+    # Clean up the connection
+    connection.close()
